66dc191ace325b4415619bd8309ae25b78cd3b6e
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-05-01 12:20:33 +0200

    Last corrections Headerjs

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -39,7 +39,8 @@ export default function Header() {
     const GradientButton = styled.button`
         color: white;
         background: rgb(255,167,66);
-        background: linear-gradient(45deg, rgba(255,167,66,1) 0%, rgba(255,45,45,1) 37%, rgba(186,48,255,1) 62%, rgba(0,232,212,1) 80%);
+        background: linear-gradient(45deg, rgba(255,167,66,1) 0%, rgba(255,45,45,1) 37%, 
+        rgba(186,48,255,1) 62%, rgba(0,232,212,1) 80%);
         border: none;
         border-radius: 30px;
         font-weight: 500;
@@ -53,19 +54,18 @@ export default function Header() {
 
     return (
         <HeaderContainer>
-        <LeftContent id="showcases">
-            <h1 style={{fontWeight:'800'}}> Rent, Play & Return </h1>
-            <div id="content" style={{margin: '5px'}}>
-            <p>What About Renting Your favourite Items  <br />
-            Insted of Spend a lot of Money Buying??<br />     
-            SuperRenting is the platform the makes this happens</p>
-            </div>
-            <GradientButton id="main-page-btn" >Start Now</GradientButton>
-        </LeftContent>
-        <RightContent>
-        <img src="/img/logo.png" alt="SR-logo" />
-        </RightContent>
+            <LeftContent id="showcases">
+                <h1 style={{fontWeight:'800'}}> Rent, Play & Return </h1>
+                    <div id="content" style={{margin: '5px'}}>
+                        <p>What About Renting Your favourite Items  <br />
+                        Insted of Spend a lot of Money Buying??<br />     
+                        SuperRenting is the platform the makes this happens</p>
+                    </div>
+                <GradientButton id="main-page-btn" >Start Now</GradientButton>
+            </LeftContent>
+                <RightContent>
+                    <img src="/img/logo.png" alt="SR-logo" />
+                </RightContent>
         </HeaderContainer>
-
     )
 }
