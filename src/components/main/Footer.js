a90f062814af3d89f0613f1c9be2d1ef9d3b9a48
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-05-01 11:55:05 +0200

    styled components first changes

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -1,58 +1,66 @@
 import React from 'react'
 import {Form, Button} from 'react-bootstrap'
+import styled from 'styled-components'
 
 
 export default function Footer() {
-    const mainFooter={
-        height:'400px',
-        margin:'3rem',
-        padding:'2rem'
-    };
 
-    const newsLetter={
-        display:'flex',
-        flexDirection:'row',
-        alignContent:'center',
-        justifyContent:'center',
-        width:'40%',
-        height:'150px',
-        padding:'2rem',
-        margin:'auto'
-    };
+    const MainFooter = styled.div`
+        margin-top: 2rem;
+        `;
 
-    const linksStyle={
-        display:'flex',
-        flexDirection:'row',
-        alignContent:'center',
-        justifyContent:'center',
-        width:'60%',
-        height:'150px',
-        margin:'auto'
-    };
+    {/* const NewsLetter = styled.div`
+        display: flex;
+        flex-direction: row;
+        align-content: center;
+        justify-content: center;
+        width: 40%;
+        height: 150px;
+        padding: 2rem;
+        margin: auto;
+    `; */}
 
-    const links ={
-        display:'flex',
-        flexDirection:'column',
-        alignContent:'center',
-        justifyContent:'center',
-        width:'30%',
-        padding:'auto',
-        margin:'5rem'
+    const LinksStyle = styled.div`
+        display: flex;
+        flex-direction: row;
+        align-content: center;
+        justify-content: center;
+        width: 60%;
+        height:150px;
+        margin: auto;
+    `;
+
+    const FooterLinks = styled.div`
+        display:flex;
+        flex-direction:column;
+        align-content: center;
+        justify-content: center;
+        width: 30%;
+        padding: auto;
+        margin: 5rem;
+        color: #fff;
+
+        & > a {
+            margin: auto;
+        }
+    `;
+
+    const CopyrightStyle = styled.div`
+        display: flex;
+        align-content: center;
+        justify-content:center;
+        padding: auto;
+        margin: auto;
         
-    };
-
-    const copyrightStyle = {
-        display:'flex',
-        alignContent:'center',
-        justifyContent:'center',
-        padding:'auto',
-        
-    };
+        & > p{
+            margin: 20px;
+        }
+    `;
 
     return (
-        <div style={mainFooter}>
+        <MainFooter>
             <div >
-            {/* <Form style={newsLetter}>
+            {/* <NewsLetter>
                 <Form.Group controlId="formBasicEmail" >
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" style={{width:'100%'}}/>
@@ -60,28 +68,29 @@ export default function Footer() {
                 <Button variant="primary" type="submit" style={{margin:'30px', height:'40px'}}>
                     Submit
                 </Button>
-            </Form> */}
+            </NewsLetter> */}
             </div>
-            <div style={linksStyle}>
-                <div style={links}>
+            <LinksStyle>
+                <FooterLinks>
                     <a>Telegram</a><p></p>
                     <a>Careers</a><p></p>
                     <a>Affiliates</a>
-                </div>
-                <div style={links}>
+                </FooterLinks>
+                <FooterLinks>
                     <a>Github</a><p></p>
                     <a>Docs</a><p></p>
                     <a>Team</a>
-                </div>
-                <div style={links}>
+                </FooterLinks>
+                <FooterLinks>
                     <a>Protocols</a><p></p>
                     <a>Partners</a><p></p>
                     <a>Gradient Paper</a>
-                    </div>
-                </div>  
-            <div style={copyrightStyle}>
+                </FooterLinks>
+                 
+             </LinksStyle>   
+            <CopyrightStyle>
             <p>SuperRenting&copy;</p>
-            </div> 
-        </div>
+            </CopyrightStyle> 
+        </MainFooter>
     )
 }
