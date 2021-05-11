e2ef76e4e48e9f7a14f7ad9d1a463da7982c02b1
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-05-11 23:42:28 +0200

    Nft datas

--- src/components/main/OpenSea.js
+++ src/components/main/OpenSea.js
@@ -7,3 +7,24 @@ const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io')
 const seaport = new OpenSeaPort(provider, {
   networkName: Network.Main
 })
+
+
+{/* Data I need to get from NFT */}
+
+{
+    "data": {
+      "id": "",
+      "walletAddress": "",
+      "contractAddress": "",
+      "tokenID": "",
+      "name": "",
+      "image": "",
+      "animation": "",
+      "schemaName": "ERC1155",
+      "currentPrice": "",
+      "quantity": "1",
+      "listedByUser": true or false
+    },
+    "username": "",
+    "handleBuy": "ƒ i() {}"
+  }
